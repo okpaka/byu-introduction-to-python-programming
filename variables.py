ca60788d7cde9488ef981e2f6bcedf5cbd44ac4c
okpaka <okpakalucky17@gmail.com>
2025-03-05 21:20:05 -0800

    variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+name = "okpaka Lucky Great"
+age = 26
+country = "Nigeria"
+Programming_languages = "Python, JavaScript, HTML, CSS, SQL, C++, C#, Php, Dart and flutter"
+
+print("My name is: ", name)
+print("I am ", age, " years old")
+print("I am from ", country)
+print("I can code in: ", Programming_languages)
+# The variables name, age, country, and Programming_languages are assigned values and then printed to the console.
