7691ba363cd877734cb6f56866d4db77a6dc831d
okpaka <okpakalucky17@gmail.com>
2025-03-05 23:26:28 -0800

    this is the madlib, i just type out the words exactly not yet edited to take input from user

---
+++ Activity/Madlib.py
@@ -0,0 +1,9 @@
+"""
+Prints out a madlib with the following words filled in:
+NOUN, ADJECTIVE, VERB, NOUN
+
+Author: Okpaka Lucky
+Date: 2025 march 7
+"""
+
+print("The other day, I was really in trouble. It all started when I saw a very happy zebra sneeze down the hallway. Hooray! I yelled. But all I could thing to do was to read over and over. Miraculously, that caused it to stop, but not before it tried to drive right in front of my family")
