c18d2ef28e6098a1468d4aefe91c12a56ae10e46
okpaka <okpakalucky17@gmail.com>
2025-03-05 21:42:09 -0800

    getting users input from the command line

---
+++ User input.py
@@ -0,0 +1,6 @@
+#create a variable called name and set it to the user's input
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+print(f"my name is {name} and my age is {age}")
