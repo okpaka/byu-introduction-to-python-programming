9c5a7592fcfb997efd8a72606e834393ff08934e
okpaka <okpakalucky17@gmail.com>
2025-03-08 09:43:14 +0200

    created a folder for each week and moved the files into the corresponding folder

---
+++ week 1/basic f-string.py
@@ -0,0 +1,5 @@
+name = "okpaka Lucky Great"
+age = 26
+
+print(f"my name is {name} and my age is {age}")
+# The variables name and age are assigned values and then printed to the console using f-string.
