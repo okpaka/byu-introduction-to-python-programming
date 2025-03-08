49d3e8cae3c95e664913e65963f9a884aaf3e2f4
okpaka <okpakalucky17@gmail.com>
2025-03-08 09:47:41 +0200

    moved files to corresponding folders

--- basic f-string.py
+++
@@ -1,5 +0,0 @@
-name = "okpaka Lucky Great"
-age = 26
-
-print(f"my name is {name} and my age is {age}")
-# The variables name and age are assigned values and then printed to the console using f-string.
