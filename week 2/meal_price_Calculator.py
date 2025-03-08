cf107b0d106c106567222f60e9dde91db2c6af1c
okpaka <okpakalucky17@gmail.com>
2025-03-08 11:00:17 +0200

    meal price calculator taking input from user

---
+++ week 2/meal_price_Calculator.py
@@ -0,0 +1,12 @@
+# This program calculates the price of a meal, 
+""" 
+Author: Okpaka Lucky Great
+Date: 2025 March 8
+"""
+
+
+# The price of the meal is stored in the variable meal_price
+child_meal_price = input("Enter the price of the child's meal: ")
+adult_meal_price = input("Enter the price of the adult's meal: ")
+num_of_children = input("Enter the number of children: ")
+num_of_adults = input("Enter the number of adults: ")
