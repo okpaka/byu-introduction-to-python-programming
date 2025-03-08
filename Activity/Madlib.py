aa475c571185b7749409247f4c26881095453a71
okpaka <okpakalucky17@gmail.com>
2025-03-08 09:27:13 +0200

    modified to take input and display the output along with the word

--- Activity/Madlib.py
+++ Activity/Madlib.py
@@ -5,5 +5,12 @@ NOUN, ADJECTIVE, VERB, NOUN
 Author: Okpaka Lucky
 Date: 2025 march 7
 """
+#create a variable for each word
+Noun = input("Enter a NOUN: ")
+Adjective = input("Enter an ADJECTIVE: ")   
+Verb = input("Enter a VERB: ")
+Noun2 = input("Enter another NOUN: ")
 
-print("The other day, I was really in trouble. It all started when I saw a very happy zebra sneeze down the hallway. Hooray! I yelled. But all I could thing to do was to read over and over. Miraculously, that caused it to stop, but not before it tried to drive right in front of my family")
+print("Your Story is: ")
+#Print the story with the words filled in
+print("The other day, I was really in trouble. It all started when I saw a very "+ Adjective +" " + Noun + " "+ Verb+ " down the hallway. Hooray! I yelled. But all I could think to do was to " + Verb + " over and over. Miraculously, that caused it to stop, but not before it tried to drive right in front of my " + Noun2)
