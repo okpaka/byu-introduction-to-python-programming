7aa72765760ac4993d479470811f922c235647bf
okpaka <okpakalucky17@gmail.com>
2025-03-09 08:22:08 +0200

    added an if statement to check if the user wants to play again ```

--- week 1/Activity/Madlib.py
+++ week 1/Activity/Madlib.py
@@ -4,13 +4,20 @@ NOUN, ADJECTIVE, VERB, NOUN
 
 Author: Okpaka Lucky
 Date: 2025 march 7
+
+I have added a review option to the madlib game
 """
 #create a variable for each word
 Noun = input("Enter a NOUN: ")
-Adjective = input("Enter an ADJECTIVE: ")   
+Adjective = input("Enter an ADJECTIVE: ")  
 Verb = input("Enter a VERB: ")
 Noun2 = input("Enter another NOUN: ")
+exclamation = "hooray!".capitalize()
 
-print("Your Story is: ")
-#Print the story with the words filled in
-print("The other day, I was really in trouble. It all started when I saw a very "+ Adjective +" " + Noun + " "+ Verb+ " down the hallway. Hooray! I yelled. But all I could think to do was to " + Verb + " over and over. Miraculously, that caused it to stop, but not before it tried to drive right in front of my " + Noun2)
+
+review = input("-----------------------Do you want to review your story? (yes/no): -----------------------")
+if review == "yes":
+    print("Your Story is: ")
+    print("The other day, I was really in trouble. It all started when I saw a very "+ Adjective +" " + Noun + " "+ Verb+ " down the hallway. "+ exclamation +" I yelled. But all I could think to do was to " + Verb + " over and over. Miraculously, that caused it to stop, but not before it tried to drive right in front of my " + Noun2)
+else:
+    print("Thank you for playing the game")
